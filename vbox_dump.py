a2e87d8d370a1d8d49f9386f100bf396b0afed42
Henrique Moreira <h@serrasqueiro.com>
2022-10-10 00:19:15 +0100

    enclose -- yet, missing recursiveness

--- vbox_dump.py
+++ vbox_dump.py
@@ -7,14 +7,23 @@ Author: Henrique Moreira, h@serrasqueiro.com
 
 # pylint: disable=missing-function-docstring
 
+import enclose
 import xparse.vbox
+from xparse.elems import SElem
 
 DEF_NAME = ""
 #DEF_NAME = "bell_SAMPLE.vbox"
 
 def main():
     fname = DEF_NAME if DEF_NAME else "D:/maquinas/vm_vbox/bell/bell.vbox"
-    xparse.vbox.main_test(fname)
+    vbx = xparse.vbox.main_test(fname)
+    tree = SElem(vbx.own())
+    encl = enclose.Enclose(tree, name=tree.string, subs=tree.own())
+    encl.parse()
+    print("==" * 20)
+    astr = encl.get_data()
+    print(astr)
+    print("==" * 20)
 
 if __name__ == "__main__":
     main()
