b75947c8ab3fb90f67a56267956ad15c918f0f3b
Henrique Moreira <h@serrasqueiro.com>
2022-10-15 21:06:37 +0100

    Using parameters, and default

--- vbox_dump.py
+++ vbox_dump.py
@@ -7,15 +7,21 @@ Author: Henrique Moreira, h@serrasqueiro.com
 
 # pylint: disable=missing-function-docstring
 
+import sys
 import xparse.vbox
 from xparse.enclosed import Enclose
 from xparse.elems import SElem
 
-DEF_NAME = ""
-#DEF_NAME = "bell_SAMPLE.vbox"
+DEF_NAME = "bell_SAMPLE.vbox"
 
 def main():
-    fname = DEF_NAME if DEF_NAME else "D:/maquinas/vm_vbox/bell/bell.vbox"
+    run_script(sys.argv[1:])
+
+def run_script(args):
+    param = args if args else [DEF_NAME]
+    fname = param[0]
+    if len(param) > 1:
+        print("Ignored extra params:", param[1:])
     vbx = xparse.vbox.main_test(fname)
     tree = SElem(vbx.own())
     encl = Enclose(tree, name=tree.string, subs=tree.own())
