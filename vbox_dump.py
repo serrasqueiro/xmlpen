df2db4333f56f9ecb0476430058c1c11119bd38c
Henrique Moreira <h@serrasqueiro.com>
2022-10-15 21:00:19 +0100

    Enclose is now at xparse/

--- vbox_dump.py
+++ vbox_dump.py
@@ -7,8 +7,8 @@ Author: Henrique Moreira, h@serrasqueiro.com
 
 # pylint: disable=missing-function-docstring
 
-import enclose
 import xparse.vbox
+from xparse.enclosed import Enclose
 from xparse.elems import SElem
 
 DEF_NAME = ""
@@ -18,7 +18,7 @@ def main():
     fname = DEF_NAME if DEF_NAME else "D:/maquinas/vm_vbox/bell/bell.vbox"
     vbx = xparse.vbox.main_test(fname)
     tree = SElem(vbx.own())
-    encl = enclose.Enclose(tree, name=tree.string, subs=tree.own())
+    encl = Enclose(tree, name=tree.string, subs=tree.own())
     encl.parse()
     print("==" * 20)
     astr = encl.get_data()
